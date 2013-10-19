639243887491c304cb10919a97e18cceb1191ebd
sato sin <shinichiro@wano.co.jp>
2013-10-19 15:58:08 +0900

    add minilla first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Time::List - It's new $module
+
+# SYNOPSIS
+
+    use Time::List;
+
+# DESCRIPTION
+
+Time::List is ...
+
+# LICENSE
+
+Copyright (C) sato sin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+sato sin <shinichiro@wano.co.jp>
---
+++ lib/Time/List.pm
@@ -0,0 +1,39 @@
+package Time::List;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Time::List - It's new $module
+
+=head1 SYNOPSIS
+
+    use Time::List;
+
+=head1 DESCRIPTION
+
+Time::List is ...
+
+=head1 LICENSE
+
+Copyright (C) sato sin.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+sato sin E<lt>shinichiro@wano.co.jpE<gt>
+
+=cut
+
